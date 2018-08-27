0526a72c3b4ce0236ecf19da354aa48d34d197b3
RandyGaul <randygaul@users.noreply.github.com>
2018-08-27 13:24:07 -0700

    chmod x some files

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -2,9 +2,9 @@
 mkdir build
 cd build
 cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="Release" ../ || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=Release
-msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=Release
+msbuild.exe aws-c-http.vcxproj /p:Configuration=Release || goto error
+msbuild.exe tests/aws-c-http-assert-tests.vcxproj /p:Configuration=Release
+msbuild.exe tests/aws-c-http-tests.vcxproj /p:Configuration=Release
 ctest -V || goto error
 
 goto :EOF
