a8f876f999adc9ff576d2ee01de4a2aec876f66f
RandyGaul <randygaul@users.noreply.github.com>
2018-08-28 17:35:05 -0700

    use henso's fix for gcc4 32bit build in s2n PR more_x86_fixes

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -44,7 +44,10 @@ if [ "$BUILD_32BIT" = true ]; then
     cd ..
 
     # Install s2n with specific lib crypto root
-    git clone https://github.com/awslabs/s2n.git
+    # For now we are grabbing Henso's workaround for gcc4 32bit build where some inline asm
+    # was using the ebx register, which is not allowed with the -fPIC flag (set in the above
+    # setarch command). Workaround in the more_x86_fixes pull request.
+    git clone https://github.com/awslabs/s2n.git -b more_x86_fixes
     cd s2n
     mkdir build
     cd build
