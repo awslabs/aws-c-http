d91ff7fa8518ab9c4a57d1bee30d03781aceed9b
Michael Graeb <graebm@amazon.com>
2019-02-07 17:01:06 -0800

    update CI scripts

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-# Until CodeBuild supports macOS, this script is just used by Travis.
-
 set -e
+
+echo "Using CC=$CC CXX=$CXX"
 
 CMAKE_ARGS="$@"
 
+# install_library <git_repo> [<commit>]
 function install_library {
     git clone https://github.com/awslabs/$1.git
     cd $1
+
+    if [ -n "$2" ]; then
+        git checkout $2
+    fi
+
     mkdir build
     cd build
 
     cmake -DCMAKE_INSTALL_PREFIX=../../install -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
     make install
-    
+
     cd ../..
 }
 
 cd ../
 
-mkdir install
+mkdir -p install
 
+# If TRAVIS_OS_NAME is OSX, skip this step (will resolve to empty string on CodeBuild)
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+    sudo apt-get install libssl-dev -y
+    install_library s2n 7c9069618e68214802ac7fbf45705d5f8b53135f
+fi
 install_library aws-c-common
 install_library aws-c-io
 
@@ -33,3 +44,5 @@ cmake -DCMAKE_INSTALL_PREFIX=../../install -DENABLE_SANITIZERS=ON $CMAKE_ARGS ..
 make
 
 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest --output-on-failure
+
+cd ..
