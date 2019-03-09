d30ff73b3c3952257070bc8101f459ee075b6e7e
Jonathan M. Henson <henso@amazon.com>
2019-03-08 16:34:15 -0800

    Elasticurl worksgit statusgit statusgit status

--- include/aws/http/exports.h
+++ include/aws/http/exports.h
@@ -17,7 +17,7 @@
  */
 
 #if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32)
-#    ifdef USE_IMPORT_EXPORT
+#    ifdef AWS_HTTP_USE_IMPORT_EXPORT
 #        ifdef AWS_HTTP_EXPORTS
 #            define AWS_HTTP_API __declspec(dllexport)
 #        else
@@ -25,10 +25,15 @@
 #        endif /* AWS_HTTP_EXPORTS */
 #    else
 #        define AWS_HTTP_API
-#    endif /* USE_IMPORT_EXPORT */
+#    endif // USE_IMPORT_EXPORT
 
-#else /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
-#    define AWS_HTTP_API
-#endif /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
+#else
+#    if ((__GNUC__ >= 4) || defined(__clang__)) && defined(AWS_HTTP_USE_IMPORT_EXPORT) && defined(AWS_HTTP_EXPORTS)
+#        define AWS_HTTP_API __attribute__((visibility("default")))
+#    else
+#        define AWS_HTTP_API
+#    endif /* __GNUC__ >= 4 || defined(__clang__) */
+
+#endif /* defined(USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32) */
 
 #endif /* AWS_HTTP_EXPORTS_H */
