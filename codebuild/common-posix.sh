1ee2b754330b9ec75cf65a02f0a10d194474add1
Justin Boswell <boswej@amazon.com>
2019-04-30 16:19:35 -0700

    try catchsegv

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -45,7 +45,7 @@ if [ $suffix_idx -gt 0 ]; then
     clang_version=${CC:$suffix_idx}
 fi
 
-LD_PRELOAD=/lib/x86_64-linux-gnu/libSegFault.so LSAN_OPTIONS=verbosity=1:log_threads=1 ctest --output-on-failure 2>&1 | llvm-symbolizer-${clang_version} -inlining -pretty-print -obj=`pwd`/tests/aws-c-http-tests
+LSAN_OPTIONS=verbosity=1:log_threads=1 catchsegv ctest --output-on-failure
 popd
 python3 integration-testing/http_client_test.py $INSTALL_PATH/bin/elasticurl
 
