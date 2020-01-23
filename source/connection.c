e25d976eed0ea184c73ad03a1d446e00a5213fa0
Bret Ambrose <bambrose@amazon.com>
2020-01-23 14:58:34 -0800

    Windows warning

--- source/connection.c
+++ source/connection.c
@@ -850,7 +850,7 @@ static const uint32_t MAX_STREAM_ID = UINT32_MAX >> 1;
 
 uint32_t aws_http_connection_get_next_stream_id(struct aws_http_connection *connection) {
 
-    uint32_t next_id = aws_atomic_fetch_add(&connection->next_stream_id, 2);
+    uint32_t next_id = (uint32_t)aws_atomic_fetch_add(&connection->next_stream_id, 2);
     /* If next fetch would overflow next_stream_id, set it to 0 */
     if (AWS_UNLIKELY(next_id > MAX_STREAM_ID)) {
         AWS_LOGF_INFO(AWS_LS_HTTP_CONNECTION, "id=%p: All available stream ids are gone", (void *)connection);
