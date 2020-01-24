4abad0db65ab744614030dbfe81851ed598c8a1b
Jonathan M. Henson <henso@amazon.com>
2020-01-23 20:14:18 -0800

    Fix cmake config template. (#178)

--- cmake/aws-c-http-config.cmake
+++ cmake/aws-c-http-config.cmake
@@ -4,8 +4,8 @@ find_dependency(aws-c-io)
 find_dependency(aws-c-compression)
 
 if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
 else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
 endif()
 
