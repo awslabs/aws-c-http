2da949ae2d8e1d8f364c8f30738a067254b36488
Dengke Tang <dengket@amazon.com>
2020-06-10 16:05:49 -0700

    udpate comments

--- include/aws/http/request_response.h
+++ include/aws/http/request_response.h
@@ -776,7 +776,7 @@ int aws_http2_stream_reset(struct aws_http_stream *http2_stream, uint32_t http2_
  * Only valid if the stream has completed, and an RST_STREAM frame has received.
  *
  * @param http2_stream HTTP/2 stream.
- * @param out_http2_error Store the http2 error code received in rst_stream.
+ * @param out_http2_error Gets to set to HTTP/2 error code received in rst_stream.
  */
 AWS_HTTP_API
 int aws_http2_stream_get_received_reset_error_code(struct aws_http_stream *http2_stream, uint32_t *out_http2_error);
@@ -786,7 +786,7 @@ int aws_http2_stream_get_received_reset_error_code(struct aws_http_stream *http2
  * Only valid if the stream has completed, and has sent an RST_STREAM frame.
  *
  * @param http2_stream HTTP/2 stream.
- * @param out_http2_error Store the http2 error code sent in rst_stream.
+ * @param out_http2_error Gets to set to HTTP/2 error code sent in rst_stream.
  */
 AWS_HTTP_API
 int aws_http2_stream_get_sent_reset_error_code(struct aws_http_stream *http2_stream, uint32_t *out_http2_error);
