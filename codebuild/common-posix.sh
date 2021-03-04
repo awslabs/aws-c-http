672ead0720e8d694843c84921bdcc6e2e5b46945
Bret Ambrose <bretambrose@gmail.com>
2021-03-04 13:43:29 -0800

    Updates

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -5,7 +5,6 @@ set -e
 if test -f "/tmp/setup_proxy_test_env.sh"; then
     source /tmp/setup_proxy_test_env.sh
     env
-    curl --verbose --proxy http://ec2-100-25-139-228.compute-1.amazonaws.com:3128 www.example.com
 fi
 
 python3 -c "from urllib.request import urlretrieve; urlretrieve('https://d19elf31gohf1l.cloudfront.net/LATEST/builder.pyz?date=`date +%s`', 'builder')"
