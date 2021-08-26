0d5cd166a6cb08aa146d0531e7d5b7f6d402ce6c
Dengke Tang <dengket@amazon.com>
2021-08-25 20:41:39 -0700

    use source?

--- integration-testing/http_proxy_env_setup.py
+++ integration-testing/http_proxy_env_setup.py
@@ -2,4 +2,4 @@ import os
 
 if os.path.exists("/tmp/setup_proxy_test_env.sh"):
     print("Setting up proxy environment")
-    os.system("/tmp/setup_proxy_test_env.sh")
+    os.system("source /tmp/setup_proxy_test_env.sh")
