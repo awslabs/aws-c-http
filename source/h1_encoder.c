5db185475c990db66d42c9c2b77330d6538de397
Itamar Levy-Or <ilevyor@amazon.com>
2021-10-12 15:10:38 -0400

    remove unnecessary logging

--- source/h1_encoder.c
+++ source/h1_encoder.c
@@ -522,22 +522,8 @@ struct aws_h1_trailer *aws_h1_trailer_new(
 
     aws_byte_buf_init(&trailer->trailer_data, allocator, trailer_size); /* cannot fail */
     s_write_headers(&trailer->trailer_data, trailing_headers);
-    AWS_LOGF_DEBUG(AWS_LS_HTTP_STREAM, "trailer size is %zu", trailer_size);
-    AWS_LOGF_DEBUG(
-        AWS_LS_HTTP_STREAM,
-        "trailer.data pre crlf len, capacity is %zu, %zu",
-        trailer->trailer_data.len,
-        trailer->trailer_data.capacity);
     if (trailer_size > 0) { /* \r\n if there are any trailers written */
-        AWS_LOGF_DEBUG(AWS_LS_HTTP_STREAM, "writing carriage return");
-        bool crlf_result = s_write_crlf(&trailer->trailer_data);
-        AWS_LOGF_DEBUG(AWS_LS_HTTP_STREAM, "crlf result is %s", crlf_result ? "true" : "false");
-        // AWS_ASSERT();
-        AWS_LOGF_DEBUG(
-            AWS_LS_HTTP_STREAM,
-            "trailer.data post crlf len, capacity is %zu, %zu",
-            trailer->trailer_data.len,
-            trailer->trailer_data.capacity);
+        s_write_crlf(&trailer->trailer_data);
     }
     return trailer;
 }
