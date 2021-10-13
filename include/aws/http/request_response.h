5551ebd3c609317b1ea3c54dc0aa125c35222be3
Justin Boswell <boswej@amazon.com>
2021-10-13 09:16:52 -0700

    Added documentation for aws_http2_stream_write_data_complete_fn

--- include/aws/http/request_response.h
+++ include/aws/http/request_response.h
@@ -377,6 +377,18 @@ struct aws_http1_chunk_options {
     void *user_data;
 };
 
+/**
+ * Invoked when the data of an outgoing HTTP2 data frame is no longer in use.
+ * This is always invoked on the HTTP connection's event-loop thread.
+ *
+ * @param stream        HTTP2-stream this write was submitted to.
+ * @param error_code    If error_code is AWS_ERROR_SUCCESS (0), the data was successfully sent.
+ *                      Any other error_code indicates that the HTTP-stream is in the process of terminating.
+ *                      If the error_code is AWS_ERROR_HTTP_STREAM_HAS_COMPLETED,
+ *                      the stream's termination has nothing to do with this write.
+ *                      Any other non-zero error code indicates a problem with this particular write's data.
+ * @param user_data     User data for this write.
+ */
 typedef aws_http_stream_write_complete_fn aws_http2_stream_write_data_complete_fn;
 
 /**
