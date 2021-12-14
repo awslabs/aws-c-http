fd86d7e56091e01baea345b0e7830e7adbdd0789
Dengke Tang <dengket@amazon.com>
2021-12-14 14:26:27 -0800

    accident

--- bin/elasticurl/main.c
+++ bin/elasticurl/main.c
@@ -410,8 +410,6 @@ static struct aws_http_message *s_build_http_request(
         aws_http2_headers_set_request_scheme(h2_headers, app_ctx->uri.scheme);
         aws_http2_headers_set_request_authority(h2_headers, app_ctx->uri.host_name);
     } else {
-        struct aws_http_headers *h2_headers = aws_http_message_get_headers(request);
-        aws_http2_headers_set_request_authority(h2_headers, app_ctx->uri.host_name);
         struct aws_http_header host_header = {
             .name = aws_byte_cursor_from_c_str("host"),
             .value = app_ctx->uri.host_name,
