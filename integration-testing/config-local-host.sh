13918bf6957f4dea8e485b2775f9f56300bc8f54
Dengke Tang <dengket@amazon.com>
2022-04-21 16:23:09 -0700

    forgot abslute dir

--- integration-testing/config-local-host.sh
+++ integration-testing/config-local-host.sh
@@ -10,12 +10,12 @@ openssl req -x509 -out localhost.crt -keyout localhost.key \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 # copy config to etc/nginx/
-sudo mv localhost.key localhost.crt etc/nginx/
+sudo mv localhost.key localhost.crt /etc/nginx/
 
 # copy the njs script
-sudo mv ./integration-testing/njs etc/nginx/
+sudo mv ./integration-testing/njs /etc/nginx/
 
 # overwrite the njs configuration
-sudo mv -f ./integration-testing/nginx.conf etc/nginx/
+sudo mv -f ./integration-testing/nginx.conf /etc/nginx/
 
 curl -k https://localhost:443/echo
