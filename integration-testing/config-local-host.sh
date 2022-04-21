65bb9d59e3bc84e7390723dcae738b3023fd2c44
Dengke Tang <dengket@amazon.com>
2022-04-21 16:36:31 -0700

    use the offical command to install nginx

--- integration-testing/config-local-host.sh
+++ integration-testing/config-local-host.sh
@@ -3,6 +3,21 @@ set -e
 
 sudo nginx -t
 
+# install njs
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | sudo tee /etc/apt/preferences.d/99nginx
+
+sudo apt update
+sudo apt install nginx nginx-module-njs
+
+
 #Generate local ssl cert
 openssl req -x509 -out localhost.crt -keyout localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
