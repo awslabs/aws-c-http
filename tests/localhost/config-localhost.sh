a78bb0fbd38743062449847738f6f146cbb51742
Dengke Tang <dengket@amazon.com>
2022-04-27 16:36:36 -0700

    let's try to build from source

--- tests/localhost/config-localhost.sh
+++ tests/localhost/config-localhost.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 set -ex
 
-# install njs
-sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+# nginx source
+curl -OL http://nginx.org/download/nginx-1.21.6.tar.gz
+tar -xvzf nginx-1.21.6.tar.gz && rm nginx-1.21.6.tar.gz
 
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-mkdir ~/.gnupg
-gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+# openssl source
+git clone -b openssl-3.0.2 https://github.com/openssl/openssl.git
 
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+# njs source
+git clone -b 0.7.3 https://github.com/nginx/njs.git
 
-sudo apt update
-sudo apt install -y nginx nginx-module-njs
+# echo module?
+git clone -b v0.62 https://github.com/openresty/echo-nginx-module.git
 
+# PCRE lib
+curl -OL https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz
+tar xvzf pcre-8.45.tar.gz
+
+# zlib
+curl -OL https://www.zlib.net/zlib-1.2.12.tar.gz
+tar xvzf zlib-1.2.12.tar.gz
+
+# configure and build
+cd nginx-1.21.6
+mkdir nginx
+./configure --add-dynamic-module=../njs/nginx --add-dynamic-module=../echo-nginx-module --with-openssl=../openssl --with-http_ssl_module --with-http_v2_module --prefix=./nginx --with-pcre=../pcre-8.45/ --with-zlib=../zlib-1.2.12
+
+make && sudo make install
 
 #Generate local ssl cert
 openssl req -x509 -out localhost.crt -keyout localhost.key \
@@ -26,19 +36,17 @@ openssl req -x509 -out localhost.crt -keyout localhost.key \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 # copy config to etc/nginx/
-sudo cp localhost.key localhost.crt /etc/nginx/
-
+cp localhost.key localhost.crt ./nginx/conf/
 # copy the njs script
-sudo cp -r ./tests/localhost/njs /etc/nginx/
-
+cp -r ./tests/localhost/njs ./nginx/conf/
 # overwrite the njs configuration
-sudo cp ./tests/localhost/nginx.conf /etc/nginx/
+cp ./tests/localhost/nginx.conf ./nginx/conf/
 
-# test the config works
-sudo nginx -t
+# check if the config works
+sudo ./nginx/sbin/nginx -t
 
-# start the service
-sudo service nginx start
+# start server
+sudo ./nginx/sbin/nginx
 
 # use curl to test if the server works
 curl -v -k https://localhost:443/echo
