5755197c31d42c4c7fc90af8f2fe2cb8879f5095
Dengke Tang <dengket@amazon.com>
2022-04-28 16:32:36 -0700

    use . to set env

--- .builder/action/aws-c-http-test.py
+++ .builder/action/aws-c-http-test.py
@@ -10,7 +10,7 @@ class AWSCHttpTest(Builder.Action):
         actions = []
         if os.path.exists('/tmp/setup_proxy_test_env.sh'):
             print("setting proxy integration test environment")
-            actions.append('/tmp/setup_proxy_test_env.sh')
+            actions.append('.', '/tmp/setup_proxy_test_env.sh')
             env.shell.setenv('AWS_PROXY_NO_VERIFY_PEER', 'on')
         if os.path.exists('./build/aws-c-http/'):
             os.chdir('./build/aws-c-http/')
@@ -20,7 +20,7 @@ class AWSCHttpTest(Builder.Action):
         localhost = False
         if os.path.exists('/tmp/setup_localhost_test.bat'):
             print("setting localhost integration test environment")
-            actions.append(['/tmp/setup_localhost_test.bat'])
+            actions.append(['.', '/tmp/setup_localhost_test.bat'])
             localhost = True
 
         if os.path.exists('/tmp/nginx-1.21.6.tar.gz') or localhost:
