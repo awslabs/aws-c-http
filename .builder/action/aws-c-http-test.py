3695a1509a676699092a79436542f5cfaafd5401
Dengke Tang <dengket@amazon.com>
2022-04-28 16:29:41 -0700

    run the script to set up envrionment?

--- .builder/action/aws-c-http-test.py
+++ .builder/action/aws-c-http-test.py
@@ -9,17 +9,18 @@ class AWSCHttpTest(Builder.Action):
     def run(self, env):
         actions = []
         if os.path.exists('/tmp/setup_proxy_test_env.sh'):
-            actions.append(['source', '/tmp/setup_proxy_test_env.sh'])
-            env.shell.setenv('AWS_PROXY_NO_VERIFY_PEER', 'on')
             print("setting proxy integration test environment")
+            actions.append('/tmp/setup_proxy_test_env.sh')
+            env.shell.setenv('AWS_PROXY_NO_VERIFY_PEER', 'on')
         if os.path.exists('./build/aws-c-http/'):
             os.chdir('./build/aws-c-http/')
         elif os.path.exists('../../aws-c-http'):
             os.chdir('../../aws-c-http')
 
         localhost = False
-        if os.path.exists('tmp/setup_localhost_test.bat'):
-            actions.append(['tmp/setup_localhost_test.bat'])
+        if os.path.exists('/tmp/setup_localhost_test.bat'):
+            print("setting localhost integration test environment")
+            actions.append(['/tmp/setup_localhost_test.bat'])
             localhost = True
 
         if os.path.exists('/tmp/nginx-1.21.6.tar.gz') or localhost:
