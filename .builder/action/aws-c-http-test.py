cc66aba0ee01db32d186bd900e7d6a2097e065cb
Dengke Tang <dengket@amazon.com>
2022-04-28 17:08:06 -0700

    I don't know...

--- .builder/action/aws-c-http-test.py
+++ .builder/action/aws-c-http-test.py
@@ -10,7 +10,7 @@ class AWSCHttpTest(Builder.Action):
         actions = []
         if os.path.exists('/tmp/setup_proxy_test_env.sh'):
             print("setting proxy integration test environment")
-            actions.append(['.', '/tmp/setup_proxy_test_env.sh'])
+            actions.append(['source', '/tmp/setup_proxy_test_env.sh'])
             env.shell.setenv('AWS_PROXY_NO_VERIFY_PEER', 'on')
         if os.path.exists('./build/aws-c-http/'):
             os.chdir('./build/aws-c-http/')
