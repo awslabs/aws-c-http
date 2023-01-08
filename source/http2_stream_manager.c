d8b876293545e09412d6042a137d61a85ef6c897
Waqar Ahmed Khan <waahm7@gmail.com>
2023-01-07 18:39:06 -0800

    Fix manual write

--- source/http2_stream_manager.c
+++ source/http2_stream_manager.c
@@ -871,6 +871,7 @@ static void s_make_request_task(struct aws_channel_task *task, void *arg, enum a
         .on_complete = s_on_stream_complete,
         .on_destroy = s_on_stream_destroy,
         .user_data = pending_stream_acquisition,
+        .http2_use_manual_data_writes = pending_stream_acquisition->options.http2_use_manual_data_writes,
     };
     /* TODO: we could put the pending acquisition back to the list if the connection is not available for new request.
      */
