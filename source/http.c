55201a6edcf40bb6dbd19c6f65c0615249a411d7
Waqar Ahmed Khan <waahm7@gmail.com>
2023-03-15 16:42:06 -0700

    throw a proper error if manual writes are not enabled

--- source/http.c
+++ source/http.c
@@ -142,6 +142,9 @@ static struct aws_error_info s_errors[] = {
     AWS_DEFINE_ERROR_INFO_HTTP(
         AWS_ERROR_HTTP_WEBSOCKET_PROTOCOL_ERROR,
         "Websocket protocol rules violated by peer"),
+    AWS_DEFINE_ERROR_INFO_HTTP(
+        AWS_ERROR_HTTP_MANUAL_WRITES_NOT_ENABLED,
+        "Manual write failed because manual writes are not enabled."),
 };
 /* clang-format on */
 
