e9cdaa47e5ac1e8bae930e3070e1b57e3a30cd80
Vera Xia <zhvxia@amazon.com>
2026-02-27 09:43:02 -0800

    clean up

--- source/connection.c
+++ source/connection.c
@@ -1184,7 +1184,6 @@ error:
 
 int aws_http_client_connect(const struct aws_http_client_connection_options *options) {
     aws_http_fatal_assert_library_initialized();
-
     if (options->prior_knowledge_http2 && options->tls_options) {
         AWS_LOGF_ERROR(AWS_LS_HTTP_CONNECTION, "static: HTTP/2 prior knowledge only works with cleartext TCP.");
         return aws_raise_error(AWS_ERROR_INVALID_ARGUMENT);
